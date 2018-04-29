25f5ad57b4739a9c8fc8351ad9dabfd23e157bcc
Scott Barnes <scottgbarnes@gwu.edu>
2018-04-29 10:44:07 -0400

    Added LQR Comments

--- optimalLQR.m
+++ optimalLQR.m
@@ -15,23 +15,19 @@
 %   G: Optimal LQR Controller
 
 function G = optimalLQR(A, B, C, D)
-    Q = C'*C; % Equally Weighs Observed Variables. Ignores those not in
-    % observer
+    Q = C'*C; % Weight Matrix: Equally Weighs Observed Variables. 
     % v = ones(1, 16); 
     % Q = diag(v); % Equally weighs all variables
-    Q(14, 14) = 1; % Pendulum Angle - Set for large displacement, small maximum angle
-    % Q(13, 13) = 0.001;
-    % Q(13, 13) = 1000; % Pendulum Displacement - Set for minimal displacement, larger angle
-    Q(1, 1) = 10; % X Position
-    Q(2, 2) = 10; % Y Position
-    Q(3, 3) = 10; % Z Position
+    Q(14, 14) = 1; % Pendulum Angle Weight
+    Q(1, 1) = 10; % X Position Weight
+    Q(2, 2) = 10; % Y Position Weight
+    Q(3, 3) = 10; % Z Position Weight
     v = ones(size(B, 2), 1);
     R = diag(v);
-    % R = 1;
-    H = [A -B*inv(R)*B'; -Q -A'];
-    [V, E] = eig(H);
+    H = [A -B*inv(R)*B'; -Q -A']; % Hamiltonain Matrix
+    [V, E] = eig(H); % Eigenavlues of Hamiltonian
     ind = 0;
-    for i = 1:size(E, 1)
+    for i = 1:size(E, 1) % Find stable eigenvalues
         if real(E(i, i)) < 0
             ind = ind + 1;
             T(:, ind) = V(:, i);
@@ -40,7 +36,7 @@ function G = optimalLQR(A, B, C, D)
     T1 = T(1:size(T,1)/2, :);
     T2 = T(size(T,1)/2+1:size(T,1), :);
     % M = T2*inv(T1);
-    M = T2*pinv(T1);
-    G = real(inv(R)*B'*M);
+    M = T2*pinv(T1); % Create M Matrix
+    G = real(inv(R)*B'*M); % Calculate Control Law
     % K = lqr(A, B, Q, R) % Uncomment to verify with MATLAB's LQR optimizer
 end
